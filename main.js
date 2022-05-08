0dd2e93d5f8ef3dae4779ce69793383ddc5b8a3e
Mohak Jain <mohakjain55055@gmail.com>
2022-05-08 09:46:15 +0530

    Working Now

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ let Operators = [document.getElementById("PlusSign"), document.getElementById("M
     document.getElementById("MultiplySign"), document.getElementById("DivideSign")];
 let EqualsButton = document.getElementById("EqualsTo");
 //Assigned all the buttons to variables and arrays
-let FirstNumber;
-let SecondNumber;
+//let FirstNumber;
+//let SecondNumber;
 // Now looping through all numbers and making OnClick function on them
 for (let i = 0; i < Numbers.length; i++) {
     Numbers[i].onclick = function () {
@@ -25,11 +25,48 @@ for (let i = 0; i < Numbers.length; i++) {
         }
     }
 }
-
-
+// Now looping through all Operators and making OnClick function on them
+for (let i = 0; i < Operators.length; i++) {
+    Operators[i].onclick = function () {
+        if (Started == false) { return; }
+        OutputLabel.innerHTML += Operators[i].innerHTML;
+    }
+}
 
 //When the equals button is clicked
 EqualsButton.onclick = function () {
-    
+    if (OutputLabel.innerHTML.includes("-")) {
+        let Output = OutputLabel.innerHTML.split("-");
+        OutputLabel.innerHTML = OperateNumb(Output[0], Output[1], "-");
+    }
+    else if (OutputLabel.innerHTML.includes("+")) {
+        let Output = OutputLabel.innerHTML.split("+");
+        OutputLabel.innerHTML = OperateNumb(Output[0], Output[1], "+");
+    }
+    else if (OutputLabel.innerHTML.includes("*")) {
+        let Output = OutputLabel.innerHTML.split("*");
+        OutputLabel.innerHTML = OperateNumb(Output[0], Output[1], "*");
+    }
+    else if (OutputLabel.innerHTML.includes("/")) {
+        let Output = OutputLabel.innerHTML.split("/");
+        OutputLabel.innerHTML = OperateNumb(Output[0], Output[1], "/");
+    }
 };
-
+function OperateNumb(FirstNumb, SecondNumb, Operator) {
+    if (Operator == "+") {
+        let Answer = Number(FirstNumb) + Number(SecondNumb);
+        return Answer;
+    }
+    else if (Operator == "-") {
+        let Answer = Number(FirstNumb) - Number(SecondNumb);
+        return Answer;
+    }
+    else if (Operator == "*") {
+        let Answer = Number(FirstNumb) * Number(SecondNumb);
+        return Answer;
+    }
+    else if (Operator == "/") {
+        let Answer = Number(FirstNumb) / Number(SecondNumb);
+        return Answer;
+    }
+}
